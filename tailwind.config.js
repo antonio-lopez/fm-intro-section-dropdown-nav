c6ce57c8ad1633d40200a544a626caf53af6be9a
Antonio Lopez <pezlop202@gmail.com>
2022-05-08 18:41:25 -0700

    initial mobile layout

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        almostWhite: 'hsl(0, 0%, 98%)',
+        mediumGray: 'hsl(0, 0%, 41%)',
+        almostBlack: 'hsl(0, 0%, 8%)',
+      },
+      fontFamily: {
+        // Weights: 500, 700
+        'Sans-serif': ['Epilogue'],
+      },
+    },
+  },
+  plugins: [],
+};
